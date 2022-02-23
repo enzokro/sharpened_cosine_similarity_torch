407b18620e4a01120fd66711a4a85c06d56acdbb
enzokro <christopher.kroenke@gmail.com>
2022-02-22 23:00:09 -0400

    add compat test for annotated conv implementation next to original conv implementation

--- compat_test.py
+++ compat_test.py
@@ -1,6 +1,6 @@
 import torch
 
-from sharpened_cosine_similarity import SharpenedCosineSimilarity, SharpenedCosineSimilarity_ConvImpl
+from sharpened_cosine_similarity import SharpenedCosineSimilarity, SharpenedCosineSimilarity_ConvImpl, SharpenedCosineSimilarity_ConvImplAnnot
 
 def test():
     original = SharpenedCosineSimilarity(5, 5, 3)
@@ -14,4 +14,16 @@ def test():
 
     print((orig_output - faster_output).abs().max().item())
 
-test()
+def test_annot():
+    original = SharpenedCosineSimilarity(5, 5, 3)
+    faster = SharpenedCosineSimilarity_ConvImplAnnot(5, 5, 3)
+    faster.load_state_dict(original.state_dict())
+
+    test_values = torch.randn(1, 5, 32, 32)
+
+    orig_output = original(test_values)
+    faster_output = faster(test_values)
+
+    print((orig_output - faster_output).abs().max().item())
+
+test(),test_annot()
