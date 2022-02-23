e077f308e215d6f95f21e6c3e4ff7e1e89f252f2
enzokro <christopher.kroenke@gmail.com>
2022-02-22 23:06:20 -0400

    small helpers to reshape weight kernels

--- compat_test.py
+++ compat_test.py
@@ -1,3 +1,4 @@
+import math
 import torch
 
 from sharpened_cosine_similarity import SharpenedCosineSimilarity, SharpenedCosineSimilarity_ConvImpl, SharpenedCosineSimilarity_ConvImplAnnot
@@ -14,10 +15,18 @@ def test():
 
     print((orig_output - faster_output).abs().max().item())
 
+def reshape_w(original):
+    state_dict = original.state_dict()
+    nin, nout, ksqr = state_dict['w'].shape
+    k = int(math.sqrt(ksqr))
+    state_dict['w'] = state_dict['w'].reshape(nin, nout, k, k)
+    return state_dict
+
 def test_annot():
     original = SharpenedCosineSimilarity(5, 5, 3)
     faster = SharpenedCosineSimilarity_ConvImplAnnot(5, 5, 3)
-    faster.load_state_dict(original.state_dict())
+    state_dict = reshape_w(original)
+    faster.load_state_dict(state_dict)
 
     test_values = torch.randn(1, 5, 32, 32)
 
